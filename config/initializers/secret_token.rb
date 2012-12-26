0237b62549b1382709f083e73d456afd76689c09
nurisa <dhimas.yudhie03@gmail.com>
2012-12-26 09:30:38 -0500

    initial commit and add the home index page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweet::Application.config.secret_token = 'c92d767d60aa6fc68fba0468620b9ab28ca30f9f3d173a7c84939aded8bb93855529eba93a146980104c09b9cda16f1ca78b3adfadee762dc0bc3b24b463864e'
